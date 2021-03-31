9c9d3cd97188034dde7915358c0694d8296adeff
akijakya <jakyandras@gmail.com>
2021-03-31 23:04:18 +0200

    Create base Header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>Task tracker</h1>
+    </header>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
